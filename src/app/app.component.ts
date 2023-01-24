6d535a7830cb96c77c3cd33b0c0f4b6d686fb937
dth <dth@provencale.lu>
2023-01-24 12:00:21 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { MatListOption, MatSelectionList } from '@angular/material/list';
+
+class Option {
+  constructor(
+    public label: string,
+    public selected: boolean
+  ) {
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mat-selection-list-2';
+  title = 'mat-selection-list';
+
+  public options: Option[] = [
+    new Option('Option 1', false),
+    new Option('Option 2', true),
+    new Option('Option 3', false)
+  ]
+
+  public updateOptions(): void {
+    this.options = [
+      new Option('Option 1', false),
+      new Option('Option 2', true),
+      new Option('Option 3', false),
+      new Option('Option 4', false),
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from "@angular/material/select";
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
